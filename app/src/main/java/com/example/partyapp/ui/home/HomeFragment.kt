9b8468b9c31560abb4ccd0ac5dc2052728fa4707
Tudor73 <tudorminciunescu@gmail.com>
2022-06-15 16:20:50 +0300

    fixed merge

--- app/src/main/java/com/example/partyapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/partyapp/ui/home/HomeFragment.kt
@@ -1,14 +1,27 @@
 package com.example.partyapp.ui.home
 
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
+import androidx.core.view.size
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.partyapp.data.Party
+import com.example.partyapp.data.User
 import com.example.partyapp.databinding.FragmentHomeBinding
+import com.example.partyapp.repositories.PartyRepository
+import kotlinx.coroutines.android.awaitFrame
+import kotlinx.coroutines.awaitCancellation
 
 class HomeFragment : Fragment() {
 
@@ -29,17 +42,56 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val pr = PartyRepository()
+        pr.getParties()
+        val listView: ListView = binding.availableParties
+        val adapter = PartyListAdapter(this.requireActivity().applicationContext)
+        listView.adapter = adapter
+        val obs = Observer<ArrayList<Party>>() {
+            Log.w("salutari", "hello!!!")
+            adapter.setNewPartyList(it)
         }
+        pr.getFinalParties().observe(viewLifecycleOwner, obs)
         return root
-
-
     }
 
     override fun onDestroyView() {
+        Log.w("salutari", "SALUT FROM DESTROY")
         super.onDestroyView()
         _binding = null
+
+    }
+
+    private class PartyListAdapter(
+        context: Context,
+    ) : BaseAdapter() {
+        var partyList: ArrayList<Party> = ArrayList<Party>()
+        private val partiesContext: Context = context
+
+        fun setNewPartyList(list: ArrayList<Party>) {
+            partyList = list
+            notifyDataSetChanged()
+        }
+
+        //      how many parties in the list
+        override fun getCount(): Int {
+            return partyList.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return partyList[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        //       renders each party/ row
+        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
+            val textView = TextView(partiesContext)
+            textView.text = partyList[position].title
+            return textView
+        }
+
     }
 }
--- app/src/main/java/com/example/partyapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/partyapp/ui/home/HomeViewModel.kt
@@ -1,13 +1,11 @@
 package com.example.partyapp.ui.home
 
+import android.provider.Telephony
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.partyapp.data.Party
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
 }
